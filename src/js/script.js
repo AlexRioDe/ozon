6b91a624b91beb9a81ebbfdb05ca916b05345c35
AlexRioDe <Make section with stocks like a ticker>
2026-07-09 23:16:34 +0300

    2

--- src/js/script.js
+++ src/js/script.js
@@ -20,4 +20,13 @@ $(document).ready(function () {
 
         return false;
     });
+
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 50) {
+            $('header').addClass('scrolled');
+        } else {
+            $('header').removeClass('scrolled');
+        }
+    });
 });
